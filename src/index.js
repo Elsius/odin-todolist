181dc80e801a2f4a4c3c689cf76704752359d4f4
Elsius <sourly95@gmail.com>
2023-01-20 20:35:44 -0800

    prettier/eslint setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const element = document.createElement('div')
+element.innerText = 'hi'
+
+document.body.appendChild(element)
+
+console.log('hihihihi')
+function testspam(){
+    console.log('sahdood')
+    console.log('testest')
+}
+testspam()
