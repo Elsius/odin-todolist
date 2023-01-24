05c8e0b63407e34a40bbfec6f0f679327f05bb3d
Elsius <sourly95@gmail.com>
2023-01-23 18:25:51 -0800

    swap init to js, begin todo logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./style.css";
+import { init } from "./initial.js";
+
+init();
--- src/initial.js
+++ src/initial.js
@@ -1,1 +1,25 @@
 // initial HTML load
+function init() {
+  document.body.innerHTML = `
+<nav>
+    <div>
+      <button>Menu</button>
+      <button>Home</button>
+      <input type="text">
+    </div>
+    <div>
+      user stuff here
+    </div>
+  </nav>
+  <sidebar>
+    <button id="todayToDo">Today</button>
+    <div id="projects">
+      <button id="viewAllProjects">View Projects</button>
+    </div>
+  </sidebar>
+  <main>
+    
+  </main>`;
+}
+
+export { init };
