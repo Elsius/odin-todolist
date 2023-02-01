d0b015b570ca768848cd71b4fffba294eec44758
Elsius <sourly95@gmail.com>
2023-01-31 21:14:21 -0800

    UI for adding new projects/tasks

--- src/todo.js
+++ src/todo.js
@@ -1,68 +1,68 @@
 import { populateTodos } from "./dom.js";
 class TodoData {
-  constructor(title, description, dueDate, priority, notes, checked) {
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.priority = priority;
-    this.notes = notes;
-    this.checked = checked;
-  }
+    constructor(title, description, dueDate, priority, notes, checked) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.checked = checked;
+    }
 }
 
 class Project {
-  constructor(title, description) {
-    this.title = title;
-    this.description = description;
-    this.tasks = [];
-  }
+    constructor(title, description) {
+        this.title = title;
+        this.description = description;
+        this.tasks = [];
+    }
 }
 
 class TodoElement {
-  constructor(todoData) {
-    const todoElement = document.createElement("div");
+    constructor(todoData) {
+        const todoElement = document.createElement("div");
 
-    const checkboxInput = document.createElement("input");
-    checkboxInput.type = "checkbox";
-    todoElement.appendChild(checkboxInput);
+        const checkboxInput = document.createElement("input");
+        checkboxInput.type = "checkbox";
+        todoElement.appendChild(checkboxInput);
 
-    const title = document.createElement("div");
-    title.textContent = todoData.title;
-    todoElement.appendChild(title);
+        const title = document.createElement("div");
+        title.textContent = todoData.title;
+        todoElement.appendChild(title);
 
-    const description = document.createElement("div");
-    description.textContent = todoData.description;
-    todoElement.appendChild(description);
+        const description = document.createElement("div");
+        description.textContent = todoData.description;
+        todoElement.appendChild(description);
 
-    const priority = document.createElement("div");
-    priority.textContent = todoData.priority;
-    todoElement.appendChild(priority);
+        const priority = document.createElement("div");
+        priority.textContent = todoData.priority;
+        todoElement.appendChild(priority);
 
-    const buttonBox = document.createElement("div");
+        const buttonBox = document.createElement("div");
 
-    const deleteButton = document.createElement("button");
-    deleteButton.classList.add("delete");
-    deleteButton.textContent = "D";
-    buttonBox.appendChild(deleteButton);
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("delete");
+        deleteButton.textContent = "D";
+        buttonBox.appendChild(deleteButton);
 
-    const editButton = document.createElement("button");
-    editButton.classList.add("edit");
-    editButton.textContent = "E";
-    buttonBox.appendChild(editButton);
+        const editButton = document.createElement("button");
+        editButton.classList.add("edit");
+        editButton.textContent = "E";
+        buttonBox.appendChild(editButton);
 
-    todoElement.appendChild(buttonBox);
+        todoElement.appendChild(buttonBox);
 
-    todoElement.classList.add("todo");
-    if (todoData.checked === true) {
-      todoElement.getElementsByTagName("input")[0].checked = true;
+        todoElement.classList.add("todo");
+        if (todoData.checked === true) {
+            todoElement.getElementsByTagName("input")[0].checked = true;
+        }
+        return todoElement;
     }
-    return todoElement;
-  }
 }
 
 function createProjectElement(projects) {
-  const projectElement = document.createElement("button");
-  projectElement.innerHTML = `
+    const projectElement = document.createElement("button");
+    projectElement.innerHTML = `
         <input type="checkbox">
         <div>
         ${projects.title}
@@ -74,15 +74,66 @@ function createProjectElement(projects) {
           <button class="delete">D</button>
           <button class="edit">E</button>
         </div>`;
-  projectElement.addEventListener("click", () => populateTodos(projects));
-  projectElement.classList.add("todo");
-  return projectElement;
+    projectElement.addEventListener("click", () => populateTodos(projects));
+    projectElement.classList.add("todo");
+    return projectElement;
 }
 
 function addItem() {
-  const container = document.createElement("button");
-  container.textContent = "New Item";
-  container.classList.add("todo");
-  return container;
+    const container = document.createElement("button");
+    container.textContent = "New Item";
+    container.classList.add("todo");
+    return container;
 }
-export { TodoData, Project, TodoElement, createProjectElement, addItem };
+
+function createItemUI(type) {
+    // if type == todo, create inputs for additional args
+    // newItemOverlay position should be detached from DOM
+    const overlayContainer = document.createElement('form')
+    overlayContainer.id = 'newItemOverlay'
+
+    const inputContainer = document.createElement('div')
+    inputContainer.classList.add('inputOverlay')
+    overlayContainer.appendChild(inputContainer)
+
+    const createInputWithLabel = (labelText, inputId, inputType = 'text') => {
+        const inputBox = document.createElement('div');
+        inputContainer.appendChild(inputBox);
+        const label = document.createElement('label');
+        label.setAttribute("for", inputId);
+        const labelTextNode = document.createTextNode(labelText);
+        label.appendChild(labelTextNode);
+        const input = document.createElement('input');
+        input.id = inputId;
+        input.type = inputType
+        inputBox.appendChild(label);
+        inputBox.appendChild(input);
+      };
+      createInputWithLabel('Title', 'inputTitle');
+      createInputWithLabel('Description', 'inputDescription');
+
+    if (type == 'todo') {
+        createInputWithLabel('Due-Date', 'inputDate')
+        createInputWithLabel('Priority','inputPriority')
+        createInputWithLabel('Notes', 'inputNotes')
+        createInputWithLabel('X','inputChecked', 'checkbox')
+        // duedate, priority, notes, checked
+    }
+    
+    const addButton = document.createElement('button')
+    addButton.textContent = 'Submit'
+    overlayContainer.appendChild(addButton)
+    addButton.addEventListener('click', function(event){
+        event.preventDefault()
+        const inputs = overlayContainer.elements
+        const inputData = {}
+        for (let i = 0; i < inputs.length; i++) {
+            inputData[inputs[i].id] = inputs[i].value;
+          }
+          console.log(inputData);
+    })
+return overlayContainer
+}
+
+// document.body.appendChild(createItemUI('todo'))
+export { TodoData, Project, TodoElement, createProjectElement, addItem , createItemUI};
